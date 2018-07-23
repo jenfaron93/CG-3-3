61a5f246ad38ac433133982d39f3f53b825a8be7
Jennifer Faron <jf891w@gmail.com>
2018-07-23 18:51:53 -0500

    Basic cSharp Skills

---
+++ BasicCSharp/BasicCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BasicCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//CSharp Basic Skills
+
+			//A statement is a line of code in C#.
+			//Multiple statements make up blocks of code.
+			//Each one of the below lines is a statement:
+
+			double width = 5.5;
+			double length = 11;
+			double area1 = width * length;
+
+			//Statements can be broken down into expressions
+			//which are a combination of operators & operands
+			//This is an expression: 
+
+			double area2 = width * length;
+
+			//Operands are the 'subject' of the statement like variables, methods, and classes.
+
+			double area3 = width * length; // the operands are area, width, and length.
+
+			//Operators are the verbs that act on the operand. 
+
+			double area4 = width * length; // the operators are = and *.
+
+
+
+
+
+
+
+
+        }
+    }
+}
